02b48101e543620c11337f71803f77e69e3f25c2
volkovakv <kv.volkova93@yandex.ru>
2022-05-05 11:11:33 +0300

    деплой проекта

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -60,7 +60,7 @@ module.exports.createUser = (req, res, next) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new RequestError('Некорректные данные пользователя'));
-      } else if (err.code === 11000) {
+      } else if (err.name === 'MongoError') {
         next(new ExistEmailError('Пользователь с таким email зарегистрирован'));
       } else {
         next(err);
